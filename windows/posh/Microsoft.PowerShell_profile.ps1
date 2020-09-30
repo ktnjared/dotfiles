4ea4b13610cf61d01225f1f0ab955d6984b7ccd6
Jared Schmidt <email@jaredschmidt.net>
2020-09-30 02:53:49 -0400

    Initial commit

---
+++ windows/posh/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,9 @@
+# Microsoft PowerShell Profile
+# File should be kept in:
+# 
+# PS❯ $mydocs = [environment]::getfolderpath("mydocuments")
+# PS❯ cd $mydocs\PowerShell
+
+Import-Module posh-git
+Import-Module oh-my-posh
+Set-Theme Paradox
